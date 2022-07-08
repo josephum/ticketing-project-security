c2bca6ff9e8e06cf92b1e6038ff0e1a791a6ca5d
Jose <josephum.garcia@gmail.com>
2022-07-08 14:03:58 -0400

    Security

--- src/main/java/com/cydeo/entity/BaseEntityListener.java
+++ src/main/java/com/cydeo/entity/BaseEntityListener.java
@@ -1,18 +1,19 @@
 package com.cydeo.entity;
 
+
 import com.cydeo.entity.common.UserPrincipal;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
 
 import javax.persistence.PrePersist;
 import javax.persistence.PreUpdate;
 import java.time.LocalDateTime;
 
+@Component
 public class BaseEntityListener extends AuditingEntityListener {
 
-    @PrePersist
     public void onPrePersist(BaseEntity baseEntity){
 
         final Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
@@ -29,7 +30,6 @@ public class BaseEntityListener extends AuditingEntityListener {
         }
     }
 
-    @PreUpdate
     public void onPreUpdate(BaseEntity baseEntity){
 
         final Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
